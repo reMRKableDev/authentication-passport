90e474996b64896e64e7d3e177a51f31fa9373b1
Malcolm Kente <malcolm.kente@remrkabledev.com>
2020-04-15 15:37:15 +0200

    Restructured tests for App component

--- client/src/components/Root/__tests__/App.test.js
+++ client/src/components/Root/__tests__/App.test.js
@@ -3,28 +3,30 @@ import App from "../App";
 import { Route } from "react-router";
 import { shallow } from "enzyme";
 import Registration from "../../Forms/Registration/Registration";
-import Login from "../../Forms/Login";
+import Login from "../../Forms/Login/Login";
 
 const shallowComponent = () => shallow(<App />); // Real unit test (isolation, no children render)
 const wrapper = shallowComponent();
 
-test("shallow render", () => {
-  expect(wrapper).toMatchSnapshot();
-});
+describe("Unit tests for App component", () => {
+  test("shallow render", () => {
+    expect(wrapper).toMatchSnapshot();
+  });
 
-test(".App styling class exists", () => {
-  expect(wrapper.find(".App").hasClass("App")).toBe(true);
-});
+  test(".App styling class exists", () => {
+    expect(wrapper.find(".App").hasClass("App")).toBe(true);
+  });
 
-test("public routes rendering the correct components", () => {
-  const routesEndpoints = wrapper
-    .find(Route)
-    .reduce((routesEndpoints, route) => {
-      const routesProps = route.props(); // Gets the routesProps from the routes
-      routesEndpoints[routesProps.path] = routesProps.component;
-      return routesEndpoints;
-    }, {});
+  test("public routes rendering the correct components", () => {
+    const routesEndpoints = wrapper
+      .find(Route)
+      .reduce((routesEndpoints, route) => {
+        const routesProps = route.props(); // Gets the routesProps from the routes
+        routesEndpoints[routesProps.path] = routesProps.component;
+        return routesEndpoints;
+      }, {});
 
-  expect(routesEndpoints["/"]).toBe(Registration);
-  expect(routesEndpoints["/login"]).toBe(Login);
+    expect(routesEndpoints["/"]).toBe(Registration);
+    expect(routesEndpoints["/login"]).toBe(Login);
+  });
 });
